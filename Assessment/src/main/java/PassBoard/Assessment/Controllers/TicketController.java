4e2909e8d88b4e77321405f4de556a08166e669a
amrassal707 <amrassal707@gmail.com>
2023-12-05 15:40:29 +0200

    adding ticketService to EventService

--- Assessment/src/main/java/PassBoard/Assessment/Controllers/TicketController.java
+++ Assessment/src/main/java/PassBoard/Assessment/Controllers/TicketController.java
@@ -4,14 +4,12 @@ import PassBoard.Assessment.DTOs.TicketDTO;
 import PassBoard.Assessment.Models.Ticket;
 import PassBoard.Assessment.Services.TicketService;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-=======
 import org.springframework.web.bind.annotation.*;
->>>>>>> 3c7b8e1 (adding ticketService to EventService)
+
 
 import java.util.List;
 
@@ -22,25 +20,21 @@ public class TicketController {
     private final TicketService ticketService;
 
 
-
     @GetMapping()
-    public List<TicketDTO> getTicket(){
+    public List<TicketDTO> getTicket() {
         return ticketService.getAll();
     }
 
-    @PostMapping
-<<<<<<< HEAD
-    public String saveTicket(TicketDTO ticketDTO) {
-=======
-    public String saveTicket(@RequestBody  TicketDTO ticketDTO) {
->>>>>>> 3c7b8e1 (adding ticketService to EventService)
+    @PostMapping()
+    public String saveTicket(@RequestBody TicketDTO ticketDTO) {
 
-        if(ticketDTO.getName().isEmpty()){
+
+        if (ticketDTO.getName().isEmpty()) {
             return "please provide a ticket name";
-        }
-        else {
+        } else {
             return ticketService.createTicket(ticketDTO);
         }
-    }
 
+
+    }
 }
