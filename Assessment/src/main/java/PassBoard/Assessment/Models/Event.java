3f8a7f133638f6a1f7a346e9888fab68bd5b735b
amrassal707 <amrassal707@gmail.com>
2023-12-06 12:35:26 +0200

    adding tickets

--- Assessment/src/main/java/PassBoard/Assessment/Models/Event.java
+++ Assessment/src/main/java/PassBoard/Assessment/Models/Event.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
-import java.util.HashMap;
+
 import java.util.List;
 
 @Document(value = "event")
