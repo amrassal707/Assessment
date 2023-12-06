8ea3d45f39ee6a5f338990e6c5830720412022b9
amrassal707 <amrassal707@gmail.com>
2023-12-06 14:20:15 +0200

    adding refund

--- Assessment/src/main/java/PassBoard/Assessment/Controllers/bookController.java
+++ Assessment/src/main/java/PassBoard/Assessment/Controllers/bookController.java
@@ -2,12 +2,10 @@ package PassBoard.Assessment.Controllers;
 
 import PassBoard.Assessment.Models.EventsPurchased;
 import PassBoard.Assessment.Services.EventPurchaseService;
-import PassBoard.Assessment.Services.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +20,14 @@ public class bookController {
     @PostMapping()
     public String bookEvent(@RequestBody EventsPurchased eventsPurchased) {
       return eventPurchaseService.bookEvent(eventsPurchased);
+    }
+    @PostMapping("/refund")
+    public String refundEvent(@RequestBody EventsPurchased eventsPurchased) {
+        return eventPurchaseService.refundEvent(eventsPurchased);
+
+    }
+    @GetMapping("/{name}")
+    public List<EventsPurchased> getEventsByName(@PathVariable String name){
+        return eventPurchaseService.getBookedEventsByName(name);
     }
 }
--- Assessment/src/main/java/PassBoard/Assessment/DAO/EventPurchaseRepo.java
+++ Assessment/src/main/java/PassBoard/Assessment/DAO/EventPurchaseRepo.java
@@ -3,5 +3,9 @@ package PassBoard.Assessment.DAO;
 import PassBoard.Assessment.Models.EventsPurchased;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface EventPurchaseRepo extends MongoRepository<EventsPurchased,String> {
+     EventsPurchased findByUserAndEventNameAndTicket(String name,String Event, String ticket);
+     List<EventsPurchased> findEventsPurchasedByUser(String name);
 }
